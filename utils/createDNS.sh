858c19674e1d803514cdc67c6c9308d6aa5c32e0
Cesar GP <cgpcsjcmp@gmail.com>
2020-11-17 21:00:42 +0100

    fix: dns script scopes and assets

--- utils/createDNS.sh
+++ utils/createDNS.sh
@@ -1,5 +1,6 @@
 curl "https://dinahosting.com/special/api.php?AUTH_USER=$1&AUTH_PWD=$2&responseType=Json&domain=$3&hostname=ui.$4&ip=$5&command=Domain_Zone_AddTypeA"
 curl "https://dinahosting.com/special/api.php?AUTH_USER=$1&AUTH_PWD=$2&responseType=Json&domain=$3&hostname=dashboard.$4&ip=$5&command=Domain_Zone_AddTypeA"
-curl "https://dinahosting.com/special/api.php?AUTH_USER=$1&AUTH_PWD=$2&responseType=Json&domain=$3&hostname=sm.$4&ip=$5&command=Domain_Zone_AddTypeA"
+curl "https://dinahosting.com/special/api.php?AUTH_USER=$1&AUTH_PWD=$2&responseType=Json&domain=$3&hostname=scopes.$4&ip=$5&command=Domain_Zone_AddTypeA"
 curl "https://dinahosting.com/special/api.php?AUTH_USER=$1&AUTH_PWD=$2&responseType=Json&domain=$3&hostname=reporter.$4&ip=$5&command=Domain_Zone_AddTypeA"
 curl "https://dinahosting.com/special/api.php?AUTH_USER=$1&AUTH_PWD=$2&responseType=Json&domain=$3&hostname=registry.$4&ip=$5&command=Domain_Zone_AddTypeA"
+curl "https://dinahosting.com/special/api.php?AUTH_USER=$1&AUTH_PWD=$2&responseType=Json&domain=$3&hostname=assets.$4&ip=$5&command=Domain_Zone_AddTypeA"
