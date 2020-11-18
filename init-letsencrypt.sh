7c8f0aa5716614c0240c0d20a57d9e502f72b3cf
Cesar GP <cgpcsjcmp@gmail.com>
2020-11-18 10:30:19 +0100

    fix: reporter env vars

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-domains=(ui.{{SERVICES_PREFIX}}{{DNS_SUFFIX}} reporter.{{SERVICES_PREFIX}}{{DNS_SUFFIX}} registry.{{SERVICES_PREFIX}}{{DNS_SUFFIX}} dashboard.{{SERVICES_PREFIX}}{{DNS_SUFFIX}} sm.{{SERVICES_PREFIX}}{{DNS_SUFFIX}})
+domains=(ui.{{SERVICES_PREFIX}}{{DNS_SUFFIX}} reporter.{{SERVICES_PREFIX}}{{DNS_SUFFIX}} registry.{{SERVICES_PREFIX}}{{DNS_SUFFIX}} dashboard.{{SERVICES_PREFIX}}{{DNS_SUFFIX}} scopes.{{SERVICES_PREFIX}}{{DNS_SUFFIX}} assets.{{SERVICES_PREFIX}}{{DNS_SUFFIX}})
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
