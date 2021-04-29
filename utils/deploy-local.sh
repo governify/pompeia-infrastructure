82549a5e3df2e9988f0c6c3249ad73a4fa46af4b
CesarGP <cgpcsjcmp@gmail.com>
2021-04-29 11:31:26 +0200

    feat: changed script to be runned from root folder

--- utils/deploy-local.sh
+++ utils/deploy-local.sh
@@ -2,14 +2,14 @@
 # Replace scope manager and assets manager key with random keys
 RANDOM_KEY1=$(openssl rand -hex 16)
 RANDOM_KEY2=$(openssl rand -hex 16)
-sed -i "s/{{RANDOM_KEY1}}/$RANDOM_KEY1/g" ./.envlocal
-sed -i "s/{{RANDOM_KEY2}}/$RANDOM_KEY2/g" ./.envlocal
+sed -i "s/{{RANDOM_KEY1}}/$RANDOM_KEY1/g" ./.env-local
+sed -i "s/{{RANDOM_KEY2}}/$RANDOM_KEY2/g" ./.env-local
 
 # Export .env variables
-export $(grep -v '^#' .env | xargs)
+export $(grep -v '^#' ./.env-local | xargs)
 
 # Create bouncer network
 docker network create governify_network
 
 # Docker compose
-docker-compose -f ./local-deploy/docker-compose-local.yaml --env-file ./local-deploy/envlocal up -d
+docker-compose -f ./docker-compose-local.yaml --env-file ./.env-local up -d
